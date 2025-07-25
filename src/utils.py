a508c596934cb0f87e699043fc0f1326d0dd7185
Facundo Cicler <cicler.facundo@gmail.com>
2025-07-25 10:36:32 -0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,6 @@
 import pymysql
 import logging
 import time
-from datetime import datetime
 
 logging.basicConfig(
     level=logging.INFO,
@@ -29,6 +28,3 @@ def wait_for_mysql(db_config: dict, timeout: int = 180) -> None:
             logging.info("Esperando a que MySQL esté disponible...")
             time.sleep(5)
     raise TimeoutError("MySQL no estuvo disponible dentro del tiempo límite.")
-
-def parse_ingestion_datetime(dt_str: str) -> datetime:
-    return datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
